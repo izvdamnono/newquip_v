13ade9bc92c1d2bd465809aec6e888d5506906c3
Kvothe96 <nono.frailes@gmail.com>
2016-10-21 00:17:39 +0200

    drawer

--- app/src/main/java/com/izv/dam/newquip/vistas/main/VistaQuip.java
+++ app/src/main/java/com/izv/dam/newquip/vistas/main/VistaQuip.java
@@ -2,15 +2,16 @@ package com.izv.dam.newquip.vistas.main;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -26,39 +27,29 @@ import com.izv.dam.newquip.pojo.Nota;
 import com.izv.dam.newquip.dialogo.DialogoBorrar;
 import com.izv.dam.newquip.vistas.notas.VistaNota;
 
-public class VistaQuip extends AppCompatActivity implements ContratoMain.InterfaceVista, OnBorrarDialogListener, NavigationView.OnNavigationItemSelectedListener {
+public class VistaQuip extends AppCompatActivity implements ContratoMain.InterfaceVista,
+        OnBorrarDialogListener,
+        NavigationView.OnNavigationItemSelectedListener {
 
     private AdaptadorNota adaptador;
     private PresentadorQuip presentador;
-
+    FloatingActionButton fab;
+    Toolbar toolbar;
+    ListView lv;
+    DrawerLayout drawer;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quip);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-
-        presentador = new PresentadorQuip(this);
-
-        ListView lv = (ListView) findViewById(R.id.lvListaNotas);
-        adaptador = new AdaptadorNota(this, null);
-        lv.setAdapter(adaptador);
+        setContentView(R.layout.activity_main);
+        init();
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(VistaQuip.this, "Editar", Toast.LENGTH_SHORT).show();
                 presentador.onEditNota(i);
             }
         });
@@ -66,19 +57,35 @@ public class VistaQuip extends AppCompatActivity implements ContratoMain.Interfa
         lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(VistaQuip.this, "delete", Toast.LENGTH_SHORT).show();
+                Toast.makeText(VistaQuip.this, "Borrar", Toast.LENGTH_SHORT).show();
                 presentador.onShowBorrarNota(i);
                 return true;
             }
         });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabAdd);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 presentador.onAddNota();
             }
         });
+    }
+
+    public void init() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        fab = (FloatingActionButton) findViewById(R.id.fabAdd);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        lv = (ListView) findViewById(R.id.lvListaNotas);
+        presentador = new PresentadorQuip(this);
+        adaptador = new AdaptadorNota(this, null);
+        lv.setAdapter(adaptador);
     }
 
     @Override
@@ -136,10 +143,12 @@ public class VistaQuip extends AppCompatActivity implements ContratoMain.Interfa
 
     }
 
-
+    /**
+     * Navigation Drawer
+     */
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -156,39 +165,33 @@ public class VistaQuip extends AppCompatActivity implements ContratoMain.Interfa
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        int id_menu = item.getItemId();
+        switch (id_menu) {
+            case R.id.nav_camera:
+                break;
+            case R.id.nav_gallery:
+                break;
+            case R.id.nav_slideshow:
+                break;
+            case R.id.nav_manage:
+                break;
+            case R.id.nav_share:
+                break;
+            case R.id.nav_send:
+                break;
+            case 0:
+                break;
+            default:
+                Log.v("MenuItem", "default");
+                break;
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
